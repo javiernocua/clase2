cb3fec451e976daf2a1fec9af28e3d50c0309b84
javiernocua <83758126+javiernocua@users.noreply.github.com>
2021-05-16 15:58:55 -0500

    Add files via upload

---
+++ retos.py
@@ -0,0 +1,27 @@
+#reto 1
+print(2+2+2-2/2) #5
+print(2+2+2+2-2) #6
+print(2+2+2+2/2) #7
+print(2*2*2*2/2) #8
+print(2*2*2+2/2) #9
+print(2+2+2+2+2) #10
+
+#reto 2
+precio_telefono=420
+precio_next_dia=precio_telefono*1.20
+print("el precio del celular al dia siguiente costara:",int(precio_next_dia))
+
+#reto 3
+vueltas_minuto=147
+segundos=640
+vueltas_segundo=vueltas_minuto/60
+vueltas_finales=vueltas_segundo*segundos
+print("En",segundos,"segundos dara",int(vueltas_finales),"vueltas")
+
+#reto 4
+cajas_refresco=9
+unidades_caja=24
+personas=56
+cantidad_refrescos=cajas_refresco*unidades_caja
+sobrante=cantidad_refrescos%personas
+print("La cantidad de refrescos sobrantes es:",sobrante)
