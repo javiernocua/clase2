bf353c0b039ed1918f87c14ff6c672fdb390dc0a
javiernocua <83758126+javiernocua@users.noreply.github.com>
2021-05-16 14:18:39 -0500

    Add files via upload

---
+++ clase2.py
@@ -0,0 +1,18 @@
+print("hola mundo")
+print("hola javier")
+print("hola","javier")
+print("110+=2")
+
+#ejercicio 2
+peso_empaque= 10
+precio_empaque= 5.95
+peso_kilo= 1000
+precio_gramo= precio_empaque/peso_empaque
+precio_kilo= precio_gramo*peso_kilo
+print("el precio por kilo es:",precio_kilo)
+
+#ejercicio 3
+peces_rojos=284
+peces_azules=163
+total_peces=peces_rojos+peces_azules
+print(total_peces)
